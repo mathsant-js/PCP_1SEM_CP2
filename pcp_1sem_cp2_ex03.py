38bdeef8ef91336e9be76fc7ae10208dd8643360
labsfiap <Nayhely.estela.c.c@gmail.com>
2026-04-22 09:49:35 -0300

    primeira alteração

---
+++ pcp_1sem_cp2_ex03.py
@@ -0,0 +1,12 @@
+print("===== CÁLCULO DA MÉDIA ===== ")
+print(" - A seguir, informe suas notas relaciondas aos Checkpoints (0 a 10): ")
+cp1 = float(input("Digite sua nota do Checkpoint 1: "))
+cp2 = float(input("Digite sua nota do Checkpoint 2: "))
+cp3 = float(input("Digite sua nota do Checkpoint 3: "))
+print(" - Agora, informe suas notas relacionadas aos Sprint e ao Global Solution (0 a 10): ")
+sp1 = float(input("Digite sua nota do Sprint 1: "))
+sp2 = float(input("Digite sua nota do Sprint 2: "))
+gs = float(input("Digite sua nota do Global Solution: "))
+
+if cp1 < cp2 < cp3:
+    media_cp_sp =
