64364a9e20bbd62ed3f9a0ddce88d8b9e4d38199
labsfiap <nayhely.estela.c.c@gmail.com>
2026-04-22 10:33:20 -0300

    adicionando a media

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -8,5 +8,14 @@ sp1 = float(input("Digite sua nota do Sprint 1: "))
 sp2 = float(input("Digite sua nota do Sprint 2: "))
 gs = float(input("Digite sua nota do Global Solution: "))
 
-if cp1 < cp2 < cp3:
-    media_cp_sp =
+if cp1 < cp2 < cp3 or cp1 < cp3 < cp2:
+    cp1 = 0
+elif cp2 < cp3 < cp1 or cp2 < cp1 < cp3:
+    cp2 = 0
+elif cp3 < cp1 < cp2 or cp3 < cp2 < cp1:
+    cp3 = 0
+
+media = ((cp1 + cp2 + cp3 + sp1 + sp2 ) / 4) * 0.4 + (gs * 0.6)
+
+mediaPeso = media * 0.4
+
